1f2be79cd720c80852c9946eb1880ed42a99c1e1
Cédric Belin <cedx@outlook.com>
2026-04-11 22:15:30 +0200

    Add the "Invoke" script

---
+++ Invoke.ps1
@@ -0,0 +1,18 @@
+#!/usr/bin/env pwsh
+param (
+	# The name of the task to invoke.
+	[Parameter(Position = 0)]
+	[ArgumentCompleter({
+		param ($commandName, $parameterName, $wordToComplete)
+		(Get-Item "$PSScriptRoot/tool/$wordToComplete*.ps1").BaseName
+	})]
+	[string] $Command = "Default",
+
+	# Value indicating whether to enable the release configuration.
+	[switch] $Release
+)
+
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+Set-StrictMode -Version Latest
+& "$PSScriptRoot/tool/$Command.ps1"
