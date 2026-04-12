5d9609c6b03814c27abb2708c1a646118a8f369d
Cédric Belin <cedx@outlook.com>
2026-04-12 13:14:07 +0200

    Update the "Outdated" script

--- tool/Outdated.ps1
+++ tool/Outdated.ps1
@@ -1,3 +1,6 @@
+using module ./PSResource.psm1
+
 "Checking for outdated dependencies..."
+(Import-PowerShellDataFile PSModules.psd1).Keys | Get-InstalledPSResource | Test-PSResourceUpdate
 dotnet package list --outdated
 npm outdated
