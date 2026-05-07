34d8f74b23df7abffca9dff1d3b26145ba17f0e4
Cédric Belin <cedx@outlook.com>
2026-05-07 21:57:42 +0200

    Update the "Lint" script

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -2,7 +2,7 @@ using module PSScriptAnalyzer
 using module ./Cmdlets.psm1
 
 "Performing the static analysis of source code..."
-$PSScriptRoot, "src", "test" | Invoke-ScriptAnalyzer -ExcludeRule PSUseShouldProcessForStateChangingFunctions -Recurse
+$PSScriptRoot, "src", "test" | Invoke-ScriptAnalyzer -ExcludeRule PSAvoidUsingPositionalParameters, PSUseShouldProcessForStateChangingFunctions -Recurse
 Test-ModuleManifest UI.psd1 | Out-Null
 
 Invoke-TypeScript "$PSScriptRoot/../src/Client/tsconfig.json" -NoEmit
